f0bed79f0075bb002bc81fd2d50b6feebe92e7ef
marioJaneta8 <mariojaneta8@gmail.com>
2026-08-24 15:29:28 -0400

    terminado Final

--- app/(routes)/(root)/teacher/analytics/page.tsx
+++ app/(routes)/(root)/teacher/analytics/page.tsx
@@ -3,20 +3,14 @@ import { auth } from "@clerk/nextjs/server";
 export const dynamic = "force-dynamic";
 
 export default async function AnalyticsPage() {
-  
-  const {userId} = await auth()
-  
+  const { userId } = await auth();
+
   if (!userId) {
     return <p>No autorizado</p>;
   }
 
-  
-
-
-    return (
-    
+  return (
     <div className="p-6">
-    
       <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
         <SuscriptorChart />
         <TotalRenevue />
